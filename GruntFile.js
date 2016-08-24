619a70e050c5b2c5c998b3dabae35ab02d80e9ee
phapdinh <centralparkloop@yahoo.com>
2016-08-24 19:53:46 -0400

    added grunt watch plugin

--- GruntFile.js
+++ GruntFile.js
@@ -1,11 +1,19 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-          uglify: {
-	          my_target: {
-		            files: { 'app.min.js': ['app.js']}  }
-		 }
+        uglify: {
+			my_target: {
+				files: { 'app.min.js': ['app.js']}  
+			}
+		},
+		watch: {
+			scripts: {
+			files: ['./*.js'],
+			tasks: ['uglify']
+			}
+		}
 	});
 
     grunt.loadNpmTasks('grunt-contrib-uglify'); // load the given tasks
+	grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['uglify']); // Default grunt tasks     
 };
