84f4c3ac540824e27a72283cdb81305f2c931981
phapdinh <centralparkloop@yahoo.com>
2016-08-25 22:17:45 -0400

    added model back to app.js and add webpack as dev dependency

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 $(function() {
-  var model = require('./model.json')
+  var model = {
+    firstCat: null,
+    secondCat: null,
+    cats: [{
+      name:"Jim",
+      url: "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426",
+      count: 0,
+	  id: "first"
+    },
+    {
+      name:"Bob",
+      url: "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
+      count: 0,
+	  id: "second"
+    },
+    {
+      name:"Paul",
+      url: "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454",
+      count: 0,
+	  id: "third"
+    },
+    {
+      name:"Anthony",
+      url: "https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg",
+      count: 0,
+	  id: "fourth"
+    },
+    {
+      name:"Andrew",
+      url: "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
+      count: 0,
+	  id: "fifth"
+    }
+    ]
+  };
   
   var controller = {
     init: function() {
